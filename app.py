7f78b8907dbb262b460271f768c906eec07eb2ad
heimatbrot@outlook.com <heimatbrot@outlook.com>
2025-02-07 19:12:26 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, join_room, leave_room, send
+
+app = Flask(__name__, template_folder='frontend')
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('join')
+def handle_join(data):
+    room = data['room']
+    join_room(room)
+    send(f"{data['username']} has joined the room {room}", to=room)
+
+@socketio.on('leave')
+def handle_leave(data):
+    room = data['room']
+    leave_room(room)
+    send(f"{data['username']} has left the room {room}", to=room)
+
+@socketio.on('message')
+def handle_message(msg):
+    room = msg['room']
+    send(msg['text'], to=room)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
---
+++ start.sh
@@ -0,0 +1,14 @@
+# filepath: start.sh
+#!/bin/bash
+
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the dependencies
+pip install -r requirements.txt
+
+# Run the application
+python3 app.py
